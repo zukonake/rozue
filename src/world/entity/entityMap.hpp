49efc45230fd5702f5fbf5c0b668b84287a483ec
zukonake <zukonake@protonmail.com>
2017-05-31 15:31:54 +0200

    EntityMap implementation

--- src/world/entity/entityMap.hpp
+++ src/world/entity/entityMap.hpp
@@ -4,30 +4,34 @@
 #include <unordered_map>
 //
 #include <world/map/typedef.hpp>
+#include <world/typedef.hpp>
 #include <world/entity/entity.hpp>
 
 namespace coldline
 {
 
 class EntitySubtype;
+class World;
 
 class EntityMap
 {
 	public:
+	EntityMap( World &world );
+
 	void simulate();
 	void moveEntity( map::Point3 const &from, map::Point3 const &to );
 
-	Entity &createEntity( EntitySubtype const &subtype, map::Point3 const &position );
-	Entity &createPlayer( EntitySubtype const &subtype );
+	Entity &createEntity( world::Position const &position, EntitySubtype const &subtype );
+	virtual Entity &createPlayer( EntitySubtype const &subtype ) = 0;
 
 	bool entityOn( map::Point3 const &point ) const;
-	bool canMove( map::Point3 const &from, map::Point3 const &to ) const;
+	bool canMove( map::Point3 const &to ) const;
 
-	Entity &getEntityOn( map::Point3 const &point );
 	Entity const &getEntityOn( map::Point3 const &point ) const;
 	private:
 	std::list< Entity > mEntities;
 	std::unordered_map< map::Point3, Entity * > mEntitiesMap;
+	World &mWorld;
 };
 
 }
