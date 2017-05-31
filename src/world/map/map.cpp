33bc775f7353c6cabab5a93dc9aeaea4d6f04dbb
zukonake <zukonake@protonmail.com>
2017-05-31 16:11:28 +0200

    Map implementation

--- src/world/map/map.cpp
+++ src/world/map/map.cpp
@@ -32,4 +32,32 @@ bool Map::sees( map::Point3 const &from, map::Point3 const &to )
 	return true;
 }
 
+bool Map::sees( map::Point3 const &from, map::Point3 const &to ) const
+{
+	if( !exists( to ) || !exists( from ))
+	{
+		return false;
+	}
+	auto plot = map::Line3( from, to ).getPlot();
+	if( plot.empty())
+	{
+		return false;
+	}
+	for( unsigned i = 1; i < plot.size() - 1; i++ )
+	{
+		if( !this->operator[]( plot[ i ]).passable())
+		{
+			return false;
+		}
+		if( entityOn( plot[ i ]))
+		{
+			if( getEntityOn( plot[ i ]).passable())
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 }
