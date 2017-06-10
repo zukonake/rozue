f5e2001e107e0b1c5ecedfa7c8e82bd23005a090
zukonake <zukonake@protonmail.com>
2017-06-10 17:42:53 +0200

    Changed Chunk::size to { 24, 24, 1 }

--- src/world/chunk/chunk.hpp
+++ src/world/chunk/chunk.hpp
@@ -23,7 +23,7 @@ class Chunk
 
 	static chunk::Point toChunkPoint( world::Point3 const &point ) noexcept;
 	static chunk::InternalPoint toInternalPoint( world::Point3 const &point ) noexcept;
-	static chunk::Size constexpr size = { 32, 32, 1 };
+	static chunk::Size constexpr size = { 24, 24, 1 };
 	private:
 	std::vector< Entity * > mEntities;
 	world::Space< size.x, size.y, size.z > mValue;
