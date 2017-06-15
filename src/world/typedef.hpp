beaad9bfb6a21ec82644cdaef9cb79213b387661
zukonake <zukonake@protonmail.com>
2017-06-15 13:21:59 +0200

    Deleted 2D World typedefs

--- src/world/typedef.hpp
+++ src/world/typedef.hpp
@@ -16,16 +16,10 @@ class Tile;
 
 typedef long long WorldCoordinate;
 typedef unsigned long long WorldLength;
-typedef ::coldline::geometry::Vector2< WorldCoordinate > WorldPoint2;
 typedef ::coldline::geometry::Vector3< WorldCoordinate > WorldPoint3;
-typedef ::coldline::geometry::Vector2< WorldCoordinate > WorldVector2;
 typedef ::coldline::geometry::Vector3< WorldCoordinate > WorldVector3;
-typedef ::coldline::geometry::Line2< WorldCoordinate > WorldLine2;
 typedef ::coldline::geometry::Line3< WorldCoordinate > WorldLine3;
-typedef ::coldline::geometry::Rectangle< WorldCoordinate, WorldLength > WorldSurfaceScope;
 typedef ::coldline::geometry::Cuboid< WorldCoordinate, WorldLength > WorldSpaceScope;
-template< WorldLength width, WorldLength height >
-using WorldSurface = ::coldline::geometry::Map2< Tile, width, height >;
 template< WorldLength width, WorldLength height, WorldLength depth >
 using WorldSpace = ::coldline::geometry::Map3< Tile, width, height, depth >;
 
