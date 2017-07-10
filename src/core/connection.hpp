f706cf68a487ba61539111c1b570ce45788f066b
zukonake <zukonake@protonmail.com>
2017-07-10 21:24:57 +0200

    Changed Connection.

--- src/core/connection.hpp
+++ src/core/connection.hpp
@@ -7,12 +7,19 @@
 
 class Client;
 
+/**
+ * Connection manages the data flow between the Client and the Server.
+ */
 class Connection
 {
 public:
 	Connection( Client &client );
 
-	void exchange();
+	/**
+	 * This funtion exchanges ClientData and ServerData between Client and Player.
+	 */
+	void exchangeData();
+	void disconnect();
 private:
 	Client &mClient;
 	Player mPlayer;
